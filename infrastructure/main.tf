b7dea7766fb0b9e7d04d2a17d5e9c56368672ee2
Riccardo Tornesello <riccardo.tornesello@gmail.com>
2025-07-10 23:36:39 +0200

    create cloud run service

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,6 +16,11 @@ resource "google_project_service" "cloudbuild" {
   service            = "cloudbuild.googleapis.com"
   disable_on_destroy = false
 }
+resource "google_project_service" "run" {
+  project            = var.project
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+}
 
 resource "google_artifact_registry_repository" "my-repo" {
   depends_on = [google_project_service.artifactregistry]
@@ -23,4 +28,19 @@ resource "google_artifact_registry_repository" "my-repo" {
   location      = var.region
   repository_id = var.repository_id
   format        = "DOCKER"
+}
+
+resource "google_cloud_run_v2_service" "default" {
+  depends_on = [google_project_service.run]
+
+  name                = "devfest-lecce-backend"
+  location            = var.region
+  deletion_protection = false
+  ingress             = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "${var.region}-docker.pkg.dev/${google_artifact_registry_repository.my-repo.project}/${google_artifact_registry_repository.my-repo.name}/devfest-lecce-backend:latest"
+    }
+  }
 }
