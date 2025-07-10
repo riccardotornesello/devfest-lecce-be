9913ec2b175b71a3d7101f0d8d64117f93ec4851
Riccardo Tornesello <riccardo.tornesello@gmail.com>
2025-07-10 17:29:22 +0200

    move app in subfolder

--- devfest_lecce_2025_be/app/settings.py
+++ devfest_lecce_2025_be/app/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "devfest_lecce_2025_be.urls"
+ROOT_URLCONF = "app.urls"
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "devfest_lecce_2025_be.wsgi.application"
+WSGI_APPLICATION = "app.wsgi.application"
 
 
 # Database
